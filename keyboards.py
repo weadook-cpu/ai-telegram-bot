cc48a4e634ef5d7ae671062644a19e9fb9300042
weadook-cpu <weadook@gmail.com>
2026-02-05 18:28:43 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,7 @@
-
+def image_generation_menu():
+    """Меню генерации изображений - ТОЛЬКО NANO BANANA"""
+    keyboard = [
+        [InlineKeyboardButton("🍌 Nano Banana", callback_data="image_nano")],
+        [InlineKeyboardButton("🔙 Geri", callback_data="back_to_main")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
